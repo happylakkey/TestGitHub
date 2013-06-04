1f8e1f5199a723f8abae304591585d19b480e4d4
lakkey <lakkey@lakkeytekiMac.local>
2013-06-04 11:49:12 +0800

    0.0.1

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by lakkey on 13-6-4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
